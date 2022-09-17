5ecff26a2933272f510281d69c088b863e5b6338
MhtRed <mou.redouan@gmail.com>
2022-09-17 21:52:01 +0100

    second refactor

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,8 @@ import axios from "axios";
 import AddButton from "../components/AddButton";
 import { useState } from "react";
 import Add from "../components/Add";
+import Product from "../models/Product";
+import dbConnect from "../utils/mongo";
 
 export default function Home({ pizzas, admin }) {
   const [close, setClose] = useState(true);
@@ -31,10 +33,14 @@ export const getServerSideProps = async (context) => {
   if (myCookie.token === process.env.NEXT_PUBLIC_ADMIN_TOKEN) {
     admin = true;
   }
-  const response = await axios.get("http://localhost:3000/api/products");
+  // const response = await axios.get("/api/products/index.js");
+  await dbConnect();
+  const res = await Product.find();
+  const pizzas = JSON.parse(JSON.stringify(res));
+  console.log({ pizzas });
   return {
     props: {
-      pizzas: response.data,
+      pizzas: pizzas,
       admin,
     },
   };
